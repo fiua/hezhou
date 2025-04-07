930717c062dd392237fc18690658ef4c96e85e54
fiua <79585497@qq.com>
2025-04-07 09:19:22 +0800

    4月7号第一次提交

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vue from '@astrojs/vue';
 // https://astro.build/config
 export default defineConfig({
-
-    vite: { // 配置 Vite
-        resolve: { // 解析模块路径
-            alias: { //设置路径别名
-                bootstrap: 'bootstrap/dist/css/bootstrap.min.css',
-            },
-        },
-    },
+    // integrations: [vue()],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                less: {
+                    additionalData: `@import "/src/style/reset.less";`
+                }
+            }
+        }
+    }
 });
--- src/api/index.js
+++ src/api/index.js
@@ -11,17 +11,41 @@ let get = async (url) => {
         console.error('There has been a problem with your fetch operation:', error);
     }
 };
-export let getNavgation = async () => {
-    return await get('/api/navigation')
+// header数据
+export let getHeader = async () => {
+    return await get('/api/header')
 }
-
-export let getCardA = async () => {
-    return await get('/api/cardA')
+//select数据
+export let getSelect = async () => {
+    return await get('/api/select')
 }
-export let getFooter = async () => {
-    return await get('/api/footer')
+//air8000对比表
+export let getAir8000Models = async () => {
+    return await get('/api/air8000_models')
 }
-
-
+//air8000所有数据
+export let getProducts8000 = async () => {
+    return await get('/api/products8000')
+}
+//4G-产品型号
+export let getProductModel = async () => {
+    return await get('/api/productModel')
+}
+// 4G-产品对比
+export let getProductcomparison = async () => {
+    return await get('/api/Productcomparison')
+}
+//功耗分析仪
+export let getMeter = async () => {
+    return await get('/api/meter')
+}
+// 云服务
+export let getCloud = async () => {
+    return await get('/api/cloud')
+}
+//联系我们
+export let getConnect = async () => {
+    return await get('/api/connect')
+}
 
 
