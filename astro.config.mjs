aa39f8b683bb48ec3f1d764dd96eef943e29ff2f
fiua <79585497@qq.com>
2025-04-14 18:56:50 +0800

    “4.14结束”

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-// https://astro.build/config
 export default defineConfig({
-    // integrations: [vue()],
+    // site: 'https://www.yoursite.com',  // 设置站点 URL
+    // output: 'static', // 生成静态站点
+    // trailingSlash: 'always', // 设置斜杠
+    compressHTML: true,
     vite: {
         css: {
             preprocessorOptions: {
                 less: {
                     additionalData: `@import "/src/style/reset.less";`
                 }
-            }
+            },
         },
+
+
     }
 });
